0f2d9a91fc0c14a387f6855e2313529fb8d63008
Theron Patrick <theronbpatrick@gmail.com>
2014-02-28 08:27:22 -0500

    Got a good start  on zooming

--- js/main.js
+++ js/main.js
@@ -3,6 +3,45 @@ $(document).ready(function() {
 
 	console.log("hi");
 
+	$(".projects").click(function() {
+		if ($(".app-container").hasClass("zoom-in")) {
+			$(".app-container").removeClass("zoom-in");
+		} else {
+			$(".app-container").addClass("zoom-in");
+		}
+
+		if ($(".zoom-container").hasClass("show")) {
+			$(".zoom-container").removeClass("show");
+		} else {
+			$(".zoom-container").addClass("show");
+		}
+
+		if ($(".block.top").hasClass("shift-top")) {
+			$(".block.top").removeClass("shift-top");
+		} else {
+			$(".block.top").addClass("shift-top");
+		}
+	});
+
+	$(".zoom-container").click(function() {
+		if ($(".app-container").hasClass("zoom-in")) {
+			$(".app-container").removeClass("zoom-in");
+		} else {
+			$(".app-container").addClass("zoom-in");
+		}
+
+		if ($(".zoom-container").hasClass("show")) {
+			$(".zoom-container").removeClass("show");
+		} else {
+			$(".zoom-container").addClass("show");
+		}
+
+		if ($(".block.top").hasClass("shift-top")) {
+			$(".block.top").removeClass("shift-top");
+		} else {
+			$(".block.top").addClass("shift-top");
+		}
+	});
 
 
 });
